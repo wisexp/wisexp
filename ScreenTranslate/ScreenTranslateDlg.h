ab7fc04908eba85f51c3402b86ebf7483e0d4f0a
wisexp <wisexp@gmail.com>
2015-11-02 02:08:49 -0800

    split char  & recgonize

--- ScreenTranslate/ScreenTranslateDlg.h
+++ ScreenTranslate/ScreenTranslateDlg.h
@@ -5,7 +5,7 @@
 #pragma once
 #include "afxwin.h"
 #include <vector>
-
+#include <unordered_map>
 
 // CScreenTranslateDlg dialog
 class CScreenTranslateDlg : public CDialogEx
@@ -35,7 +35,29 @@ public:
     CComboBox m_programs;
     afx_msg void OnBnClickedButtonRefresh();
     afx_msg void OnBnClickedButton2();
-    afx_msg void OnTimer(UINT_PTR nIDEvent);
 
-    std::vector<std::vector<COLORREF>> m_matrix;
+    bool ReadCharacter(std::vector<std::pair<int, int>>& character, wchar_t& c);
+    void SaveCharacter(std::vector<std::pair<int, int>>& character, wchar_t c);
+
+    std::vector<std::vector<COLORREF>> m_lastmatrix;
+    std::vector<std::vector<COLORREF>> m_currentmatrix;
+    std::vector<std::vector<COLORREF>> m_diff;
+    typedef std::vector<std::pair<int, int>> Char_T;
+    typedef std::vector<Char_T> Line_T;
+    std::vector<Line_T> m_characters;
+    std::vector<Line_T>::iterator m_lineIt;
+    Line_T::iterator m_charIt;
+    size_t m_width;
+    size_t m_height;
+    int m_left, m_right, m_top, m_bottom;
+    afx_msg void OnBnClickedButtonNext();
+    afx_msg void OnBnClickedButtonConfirm();
+    struct Char_Hash
+    {
+        int operator()(const Char_T& character);
+    };
+    std::unordered_map<Char_T, wchar_t, Char_Hash> m_charmap;
+    
+    CEdit m_char;
+    afx_msg void OnBnClickedButtonShowChar();
 };
